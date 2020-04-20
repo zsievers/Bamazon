bca815931dbc0e8f008abb1b9b701b03edd2380e
Zach <zjsievers@gmail.com>
2020-04-20 14:15:09 -0400

    query all products to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,8 +21,20 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  // connection.end();
+  queryAllProducts();  
+  // queryLearningProducts();
 });
 
+function queryAllProducts(){
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for(var i = 0; i < res.length; i++) {
+      console.log("\nProduct ID: " +res[i].item_id + " | Title: " +res[i].product_name + " | Category: " +res[i].department_name + " | Price: $"+res[i].price+ " | Quantity: " +res[i].stock_quantity + "\n");
+    }
+    console.log("\n");
+  });
+  // console.log(query.sql);
+  connection.end();
+}
 
   
