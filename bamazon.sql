7774a5b4d7934fb069b502d2542fab499ee9c1bf
Zach <zjsievers@gmail.com>
2020-04-20 13:18:16 -0400

    insert into products table 10 items

--- bamazon.sql
+++ bamazon.sql
@@ -10,3 +10,33 @@ CREATE TABLE products (
     stock_quantity INT NULL,
     PRIMARY KEY (item_id)
 )
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Silence of Adam", "Learning", 15 , 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Intro to Node", "Learning", 35 , 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("My SQL for Dummies", "Learning", 8 , 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iphone case", "Electronics", 8 , 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Headphones", "Electronics", 70 , 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Roomba", "Electronics", 200 , 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Catan", "Board Games", 40 , 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Parcheesi", "Board Games", 30 , 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Secret Hitler", "Board Games", 25 , 9);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Scrabble", "Board Games", 20 , 20);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  // connection.end();
 });
 
 
